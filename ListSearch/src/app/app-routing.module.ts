1fb1a09c6abf174d90292a512a1ace7875b3b368
goutham krishna <gouthamkrishnajs@outlook.com>
2022-10-09 17:23:25 +0530

    login basic changes

--- ListSearch/src/app/app-routing.module.ts
+++ ListSearch/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch:'full', redirectTo: 'login'}, ///anything will direct from app component to login component
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

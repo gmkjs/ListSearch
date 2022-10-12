a7bff4d21aed31b809db125fefbd423f1e6b554e
gouthamkrishna <gouthamkrishnajs@outlook.com>
2022-10-12 21:12:18 +0530

    testing

---
+++ ListSearch/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path: '', pathMatch:'full', redirectTo: 'login'}, ///anything will direct from app component to login component
+  {path: 'login', component: LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

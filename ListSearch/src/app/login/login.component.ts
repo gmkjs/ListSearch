91a8207d5e80cf609240c51c6274b785306135ca
gouthamkrishna <gouthamkrishnajs@outlook.com>
2022-10-12 21:12:41 +0530

    text message

--- ListSearch/src/app/login/login.component.ts
+++ ListSearch/src/app/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+   name:string="Murali";
+
   constructor() { }
+
+  
 
   ngOnInit(): void {
   }
 
 }
+
+

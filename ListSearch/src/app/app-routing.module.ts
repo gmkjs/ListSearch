c04dc8a7e5b430918854c03c9cd087cebe6a68c2
chmkmurali <chmkmuralikrishna@gmail.com>
2023-02-25 15:45:16 +0530

    Created Navbar

--- ListSearch/src/app/app-routing.module.ts
+++ ListSearch/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
+import { CareersComponent } from './careers/careers.component';
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
@@ -8,7 +11,10 @@ const routes: Routes = [
   // {path: '', pathMatch:'full', redirectTo: 'login'}, ///anything will direct from app component to login component
   
   {path: '', component: LoginComponent},
-  {path:'home', component:HomeComponent}
+  {path:'home', component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'carrers', component:CareersComponent}
 
 ];
 
